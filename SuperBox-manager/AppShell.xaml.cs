c689206df500cd59f45f7a596b44c582d302bf63
Marc Gherzan <marc.gherzan16@gmail.com>
2025-01-04 19:12:54 +0200

    plus

---
+++ SuperBox-manager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperBox_manager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SuperBox-manager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SuperBox_manager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
